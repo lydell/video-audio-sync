c9b562d1f961f0f18468f21d81f7bf619d124cf0
Simon Lydell <simon.lydell@gmail.com>
2018-05-02 23:18:17 +0200

    Add more ESLint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 const baseRules = require("eslint-config-lydell");
 
 module.exports = {
-  plugins: ["prettier"],
+  plugins: ["import", "prettier", "sort-imports-es6-autofix"],
   parser: "babel-eslint",
   env: { es6: true },
-  rules: Object.assign({}, baseRules(), {
+  rules: Object.assign({}, baseRules({ import: true }), {
     "prettier/prettier": "error",
+    "sort-imports-es6-autofix/sort-imports-es6": "error",
   }),
   globals: {
     DEBUG: false,
