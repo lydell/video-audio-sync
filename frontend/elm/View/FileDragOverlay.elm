f4ac6728cbfc3dd60093949278d3c1e043825c7a
Simon Lydell <simon.lydell@gmail.com>
2018-06-16 11:32:28 +0200

    Experiment with using Feather icons

--- frontend/elm/View/FileDragOverlay.elm
+++ frontend/elm/View/FileDragOverlay.elm
@@ -2,13 +2,14 @@ module View.FileDragOverlay exposing (fileDragOverlay)
 
 import Html exposing (Html, div, p, text)
 import Html.Attributes exposing (class)
-import View.Fontawesome exposing (Icon(CustomIcon), fontawesome)
+import View.Fontawesome exposing (Icon(Icon), fontawesome)
 
 
 fileDragOverlay : Html msg
 fileDragOverlay =
     div [ class "FileDragOverlay" ]
-        [ fontawesome (CustomIcon "copy" "fa-4x")
+        [ div [ class "FileDragOverlay-icon" ]
+            [ fontawesome (Icon "copy") ]
         , p []
             [ text "Drop video, audio and/or points" ]
         ]
--- frontend/elm/View/Fontawesome.elm
+++ frontend/elm/View/Fontawesome.elm
@@ -2,6 +2,7 @@ module View.Fontawesome exposing (Icon(..), fontawesome)
 
 import Html exposing (Html, span)
 import Html.Attributes exposing (attribute, class)
+import View.Icons as Icons
 
 
 type Icon
@@ -20,8 +21,73 @@ fontawesome icon =
                 CustomIcon name extra ->
                     ( name, extra )
     in
-    span
-        [ attribute "aria-hidden" "true"
-        , class ("fas fa-" ++ namePart ++ " " ++ extraClass)
-        ]
-        []
+    case namePart of
+        "file-video" ->
+            Icons.film
+
+        "file-audio" ->
+            Icons.volume2
+
+        "file-alt" ->
+            Icons.fileText
+
+        "play" ->
+            Icons.play
+
+        "pause" ->
+            Icons.pause
+
+        "play-circle" ->
+            Icons.playCircle
+
+        "pause-circle" ->
+            Icons.pauseCircle
+
+        "backward" ->
+            Icons.rewind
+
+        "forward" ->
+            Icons.fastForward
+
+        "step-backward" ->
+            Icons.skipBack
+
+        "step-forward" ->
+            Icons.skipForward
+
+        "sync-alt" ->
+            Icons.repeat
+
+        "plus" ->
+            Icons.plus
+
+        "minus" ->
+            Icons.minus
+
+        "save" ->
+            Icons.save
+
+        "trash" ->
+            Icons.trash2
+
+        "copy" ->
+            Icons.copy
+
+        "cog" ->
+            Icons.settings
+
+        "question-circle" ->
+            Icons.helpCircle
+
+        "keyboard" ->
+            Icons.command
+
+        "exclamation-triangle " ->
+            Icons.alertTriangle
+
+        _ ->
+            span
+                [ attribute "aria-hidden" "true"
+                , class ("fas fa-" ++ namePart ++ " " ++ extraClass)
+                ]
+                []
